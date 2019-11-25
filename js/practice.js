be3804a10f919193a7256a5ff6dfd7edfc1b653c
LLiam Smith <liamjs100@gmail.com>
2019-11-25 11:48:34 +0100

    moving files

---
+++ js/practice.js
@@ -0,0 +1,17 @@
+// alert("fuck off");
+
+let myList = [["crisps", 5],["shoes", 10],["dogs", 5]];
+
+console.log(myList);
+
+function Myfunction(){
+  console.log("This is my function");
+}
+
+Myfunction();
+
+function functionWithArgs(a, b){
+  console.log(a + b);
+}
+
+functionWithArgs("I love ", "crisps");
